91e137ca71cfd7a3675acb815995f2ee05ba9ab9
wat1881mayer <happymayer2013@icloud.com>
2021-08-09 11:39:23 +0900

    text add

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ const Error = () => {
              <main className="error-page">
                  <section>
                      <h1>404</h1>
-                     <h3>page not found,sorry</h3>
+                     <h3>page not found,sorry!!!</h3>
                  </section>
              </main>
         </Layout>
